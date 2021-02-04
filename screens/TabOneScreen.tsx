e5d261a61c9bfa901ad7b0468bb8fb68d9d44b93
Jon <35509879+JonParrish@users.noreply.github.com>
2021-02-04 12:13:51 -0700

    Add files via upload

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
-import StockList from '../components/StockList'
+
+import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <StockList/>
+      <Text style={styles.title}>Tab One</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
 }
