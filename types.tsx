7125db3ff543523caaffa940ac17a13a1d7ce883
Jon <35509879+JonParrish@users.noreply.github.com>
2021-02-04 12:19:24 -0700

    Add files via upload

---
+++ types.tsx
@@ -0,0 +1,17 @@
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  TabOne: undefined;
+  StockLookupTab: undefined;
+};
+
+export type TabOneParamList = {
+  TabOneScreen: undefined;
+};
+
+export type StockLookupTabParamList = {
+  StockLookup: undefined;
+};
